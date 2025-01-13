958aeadc2bf62354efc7b0981a43556e92e69569
kausar ahammed <kausar.a.pro@gmail.com>
2025-01-13 23:01:53 +0600

    added login from

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,21 +1,20 @@
-import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import ErrorPage from "../pages/ErrorPage";
+import Login from "../Page/Login";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element:<MainLayout></MainLayout>,
-      errorElement: <ErrorPage></ErrorPage>,
-      children:[
-        {
-          path:"/"
-        }
-      ]
-    },
-  ]);
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/login",
+        element:<Login></Login>,
+      },
+    ],
+  },
+]);
 
 export default router;
