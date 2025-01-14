d75f314f9b8b67ae4c16c1522c03e09c7d8404a2
kausar ahammed <kausar.a.pro@gmail.com>
2025-01-15 00:31:59 +0600

    added redmin route

--- src/Page/Dashboard/AllUser/AllUser.jsx
+++ src/Page/Dashboard/AllUser/AllUser.jsx
@@ -12,50 +12,48 @@ const AllUser = () => {
       const res = await axiosSecure.get("/users");
       return res.data;
     },
-  });
+  }); 
 
-  const handleMakeAdmin = user => {
-    axiosSecure.patch(`/users/admin/${user._id}`)
-    .then(res => {
-        console.log(res.data)
-        if(res.data.modifiedCount > 0){
-            refetch()
-            Swal.fire({
-                position: "top-end",
-                icon: "success",
-                title:`${user.name} is an Admin Now!`,
-                showConfirmButton: false,
-                timer: 1500
-              });
-        }
-    })
-  }
+  const handleMakeAdmin = (user) => {
+    axiosSecure.patch(`/users/admin/${user._id}`).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.name} is an Admin Now!`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
 
-  const handleDeleteUser = (user) =>{
+  const handleDeleteUser = (user) => {
     Swal.fire({
-          title: "Are you sure?",
-          text: "You won't be able to revert this!",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, delete it!",
-        }).then((result) => {
-          if (result.isConfirmed) {
-            axiosSecure.delete(`/users/${user._id}`).then((res) => {
-              console.log(res.data);
-              if (res.data.deletedCount > 0) {
-                refetch()
-                Swal.fire({
-                  title: "Deleted!",
-                  text: "User has been deleted.",
-                  icon: "success",
-                });
-              }
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/users/${user._id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "User has been deleted.",
+              icon: "success",
             });
           }
         });
-  }
+      }
+    });
+  };
   return (
     <div>
       <div className="flex justify-between my-4">
@@ -82,15 +80,16 @@ const AllUser = () => {
                 <td>{user.name}</td>
                 <td>{user.email}</td>
                 <td>
-                {
-                    user.role === "admin" ? 'Admin' :
+                  {user.role === "admin" ? (
+                    "Admin"
+                  ) : (
                     <button
-                    onClick={() => handleMakeAdmin(user)}
-                    className="hover:text-red-500 btn-lg btn bg-orange-400 text-white"
-                  >
-                    <FaUsers></FaUsers>
-                  </button>
-                }
+                      onClick={() => handleMakeAdmin(user)}
+                      className="hover:text-red-500 btn-lg btn bg-orange-400 text-white"
+                    >
+                      <FaUsers></FaUsers>
+                    </button>
+                  )}
                 </td>
                 <td>
                   <button
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -8,12 +8,11 @@ import {
   signInWithPopup,
   signOut,
   updateProfile,
- 
 } from "firebase/auth";
 import { auth } from "../firebase/firebase.config";
 import axios from "axios";
- 
- 
+import useAxiosOpen from "../hooks/useAxiosOpen";
+
 export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
@@ -22,51 +21,58 @@ const AuthProvider = ({ children }) => {
   const [resetEmail, setResetEmail] = useState("");
   const [loading, setLoading] = useState(true);
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
-  const googleProvider = new GoogleAuthProvider()
- 
+  const googleProvider = new GoogleAuthProvider();
+  const axiosOpen = useAxiosOpen();
   const handleResister = (email, password) => {
     setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
- 
+
   const handleSignIn = (email, password) => {
     setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
-  const handleGoogleLogin =()=>{
+  const handleGoogleLogin = () => {
     setLoading(true);
-    return signInWithPopup(auth, googleProvider)
-  }
+    return signInWithPopup(auth, googleProvider);
+  };
 
   const manageProfile = async (name, photo) => {
     try {
       await updateProfile(auth.currentUser, {
         displayName: name,
         photoURL: photo,
-        
       });
-      
     } catch (err) {
       toast.error(err.message);
     }
   };
 
-  const handleSignOut = ()=>{
+  const handleSignOut = () => {
     setLoading(true);
-    return signOut(auth)
- 
- }
- useEffect(() => {
+    return signOut(auth);
+  };
+  useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
-        setUser(currentUser);
-        console.log(currentUser)   
+      setUser(currentUser);
+
+      if (currentUser) {
+        // get token and store client
+        const userInfo = { email: currentUser.email };
+        axiosOpen.post("/jwt", userInfo).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+          }
+        });
+      } else {
+        localStorage.removeItem("access-token");
+      }
     });
     return () => {
-        unSubscribe();
+      unSubscribe();
     };
-}, []);
-
+  }, []);
 
   const authInfo = {
     theme,
@@ -84,7 +90,7 @@ const AuthProvider = ({ children }) => {
     setResetEmail,
     handleGoogleLogin,
     manageProfile,
-    handleSignOut
+    handleSignOut,
   };
 
   return (
